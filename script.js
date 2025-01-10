d76ecce7da3378a0c01a2aaf41252bb0231ad3cb
tlesk0708 <tlesk0708@gmail.com>
2025-01-11 04:41:55 +0700

    ตกแต่ง+ค่าในแบบทดสอบ+แถบด้านบน

---
+++ script.js
@@ -0,0 +1,11 @@
+// แสดงกล่องข้อมูล
+function showInfo() {
+    const infoBox = document.getElementById("info-box");
+    infoBox.style.display = "block"; // เปลี่ยนการแสดงผล
+}
+
+// ซ่อนกล่องข้อมูล
+function closeInfo() {
+    const infoBox = document.getElementById("info-box");
+    infoBox.style.display = "none"; // ซ่อนกล่องข้อมูล
+}
