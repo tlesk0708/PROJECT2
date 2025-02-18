8495288d38f1ad9cf5be23155749a5cabfdda5fb
tlesk0708 <tlesk0708@gmail.com>
2025-02-19 01:24:15 +0700

    database

--- project_db.sql
+++ project_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2025 at 07:27 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Feb 18, 2025 at 07:23 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,56 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `answers`
---
-
-CREATE TABLE `answers` (
-  `id` int(11) NOT NULL,
-  `question1` varchar(255) DEFAULT NULL,
-  `question2` varchar(255) DEFAULT NULL,
-  `question3` varchar(255) DEFAULT NULL,
-  `question4` varchar(255) DEFAULT NULL,
-  `question5` varchar(255) DEFAULT NULL,
-  `question6` varchar(255) DEFAULT NULL,
-  `question7` varchar(255) DEFAULT NULL,
-  `question8` varchar(255) DEFAULT NULL,
-  `question9` varchar(255) DEFAULT NULL,
-  `question10` varchar(255) DEFAULT NULL,
-  `question11` varchar(255) DEFAULT NULL,
-  `question12` varchar(255) DEFAULT NULL,
-  `question13` varchar(255) DEFAULT NULL,
-  `question14` varchar(255) DEFAULT NULL,
-  `question15` varchar(255) DEFAULT NULL,
-  `question16` varchar(255) DEFAULT NULL,
-  `question17` varchar(255) DEFAULT NULL,
-  `question18` varchar(255) DEFAULT NULL,
-  `question19` varchar(255) DEFAULT NULL,
-  `question20` varchar(255) DEFAULT NULL,
-  `question21` varchar(255) DEFAULT NULL,
-  `question22` varchar(255) DEFAULT NULL,
-  `question23` varchar(255) DEFAULT NULL,
-  `question24` varchar(255) DEFAULT NULL,
-  `question25` varchar(255) DEFAULT NULL,
-  `question26` varchar(255) DEFAULT NULL,
-  `question27` varchar(255) DEFAULT NULL,
-  `question28` varchar(255) DEFAULT NULL,
-  `question29` varchar(255) DEFAULT NULL,
-  `question30` varchar(255) DEFAULT NULL,
-  `question31` varchar(255) DEFAULT NULL,
-  `question32` varchar(255) DEFAULT NULL,
-  `question33` varchar(255) DEFAULT NULL,
-  `question34` varchar(255) DEFAULT NULL,
-  `question35` varchar(255) DEFAULT NULL,
-  `question36` varchar(255) DEFAULT NULL,
-  `question37` varchar(255) DEFAULT NULL,
-  `question38` varchar(255) DEFAULT NULL,
-  `question39` varchar(255) DEFAULT NULL,
-  `question40` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `assessment`
 --
 
@@ -83,6 +33,13 @@ CREATE TABLE `assessment` (
   `assessment2` varchar(255) NOT NULL,
   `assessment3` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `assessment`
+--
+
+INSERT INTO `assessment` (`assessment_id`, `assessment1`, `assessment2`, `assessment3`) VALUES
+(1, 'เห็นด้วยที่สุด', 'เห็นด้วยที่สุด', 'ชอบมาก');
 
 -- --------------------------------------------------------
 
@@ -100,28 +57,8 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`username`, `user_id`, `password`, `firstname`, `lastname`, `age`) VALUES
-('Tong', 1, '1234', 'สรวิศ', 'เลอธนกุล', 30),
-('Toong', 3, '1234', 'rwar', 'rawra', 30),
-('Sorawit', 10, '1234', 'baba', 'nana', 20),
-('tt', 30, 'tttt1234', 'tt', 'tt', 22),
-('t', 31, 'tttt1234', 't', 't', 22),
-('jame', 32, 'tong15632', 'นาย ดำ', ' น้ำเงิน', 17),
-('how', 33, 'fghj1234', 'สรวิศ', 'tt', 22),
-('test', 34, 'test1234', 'tt', 'tt', 22);
-
---
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `answers`
---
-ALTER TABLE `answers`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `assessment`
@@ -133,30 +70,23 @@ ALTER TABLE `assessment`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`) USING BTREE,
-  ADD UNIQUE KEY `username` (`username`) USING BTREE;
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `answers`
---
-ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `assessment`
 --
 ALTER TABLE `assessment`
-  MODIFY `assessment_id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `assessment_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
